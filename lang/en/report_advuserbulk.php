5fb9c7e566f82bad46c68ce834925436228ccd6b
Artem Andreev <andreev.artem@gmail.com>
2011-08-26 17:50:55 +0400

    Language strings fixes

--- lang/en/report_advuserbulk.php
+++ lang/en/report_advuserbulk.php
@@ -1,5 +1,6 @@
 <?php
-    $string['title'] = 'Advanced bulk user actions';
-    $string['pluginname'] = 'Advanced bulk user actions';
-    $string['advuserbulk:view'] = 'Bulk user actions';
-?>
+
+$string['title'] = 'Advanced bulk user actions';
+$string['nointernalenrol'] = 'Courses {$a} don\'t contain enrolment method «Manual enrolments»';
+$string['pluginname'] = 'Advanced bulk user actions';
+$string['advuserbulk:view'] = 'Bulk user actions';
