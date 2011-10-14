4516b472d897a516015a775477fd7daeacff3da9
Artem Andreev <andreev.artem@gmail.com>
2011-10-14 15:32:23 +0400

    Implemented action 'Assign role in course': for assigning roles with capability 'moodle/course:view'

---
+++ actions/assignroleincourse/lang/en/bulkuseractions_assignroleincourse.php
@@ -0,0 +1,14 @@
+<?PHP
+
+$string['pluginname'] = 'Assign role in courses';
+$string['accept'] = 'Accept';
+$string['allcourses'] = 'All courses';
+$string['confirmpart1'] = 'Are you sure you want to assign role ';
+$string['confirmpart2'] = '<br />to users:<br />';
+$string['confirmpart3'] = '<br />on courses:<br />';
+$string['removeall'] = 'Remove all from selection';
+$string['roletoset'] = 'Role to set<br />';
+$string['selectedcourses'] = 'Selected courses';
+$string['title'] = 'Bulk roles assignment';
+
+?>
---
+++ actions/assignroleincourse/lang/ru/bulkuseractions_assignroleincourse.php
@@ -0,0 +1,14 @@
+<?PHP
+
+$string['pluginname'] = 'Назначить роль в курсах';
+$string['allcourses'] = 'Все курсы';
+$string['accept'] = 'Принять';
+$string['confirmpart1'] = 'Вы уверены, что хотите назначить роль ';
+$string['confirmpart2'] = '<br />пользователям:<br />';
+$string['confirmpart3'] = '<br />в курсах:<br />';
+$string['removeall'] = 'Удалить все из выбранных';
+$string['roletoset'] = 'Устанавливаемая роль<br />';
+$string['selectedcourses'] = 'Выбранные курсы';
+$string['title'] = 'Пакетное назначение ролей';
+
+?>
