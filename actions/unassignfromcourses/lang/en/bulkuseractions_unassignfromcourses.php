724974ce1041c9955808829933f4709f4bdc7ead
Artem Andreev <andreev.artem@gmail.com>
2011-10-15 13:44:21 +0400

    Implemented action 'Unassing roles in courses' for removing roles assignments (with capability 'moodle/course:view') from courses

---
+++ actions/unassignfromcourses/lang/en/bulkuseractions_unassignfromcourses.php
@@ -0,0 +1,12 @@
+<?PHP
+
+$string['pluginname'] = 'Unassing roles in courses';
+$string['accept'] = 'Accept';
+$string['allcourses'] = 'All courses';
+$string['confirmpart1'] = 'Are you sure you want to unassign roles for users:<br />';
+$string['confirmpart2'] = '<br />in courses:<br />';
+$string['removeall'] = 'Remove all from selection';
+$string['selectedcourses'] = 'Selected courses';
+$string['title'] = 'Unassing roles in courses';
+
+?>
