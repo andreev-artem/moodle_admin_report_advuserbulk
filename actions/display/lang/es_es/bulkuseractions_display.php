cd7e3fc180cebaa0f54b6db3598f6855aa48bf9c
Carlos Utrera <info@carlosutrera.com>
2011-10-28 22:00:26 +0200

    Signed-off-by: Carlos Utrera <info@carlosutrera.com>

---
+++ actions/display/lang/es_es/bulkuseractions_display.php
@@ -0,0 +1,5 @@
+<?PHP
+
+$string['pluginname'] = 'Mostrar en la página';
+
+?>
---
+++ lang/es_es/report_advuserbulk.php
@@ -0,0 +1,6 @@
+<?php
+
+$string['title'] = 'Acciones de Usuario Masivas Avanzadas';
+$string['nointernalenrol'] = 'Los cursos {$a} no tienen método de matriculación «Matriculación Manual»';
+$string['pluginname'] = 'Acciones de Usuario Masivas Avanzadas';
+$string['advuserbulk:view'] = 'Acciones de Usuario Masivas';
