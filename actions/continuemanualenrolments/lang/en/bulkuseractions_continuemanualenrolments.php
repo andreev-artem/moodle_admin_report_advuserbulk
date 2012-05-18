d075cb7d65773ccdc4993cdeabfd0a8f254c6f60
Artem Andreev <andreev.artem@gmail.com>
2012-05-18 14:36:27 +0400

    Added action 'Continue manual enrolments'

---
+++ actions/continuemanualenrolments/lang/en/bulkuseractions_continuemanualenrolments.php
@@ -0,0 +1,12 @@
+<?PHP
+
+$string['pluginname'] = 'Continue manual enrolments of users';
+$string['accept'] = 'Accept';
+$string['allcourses'] = 'All courses';
+$string['confirmpart1'] = 'Are you sure you want to continue manual enrolments of users:<br />';
+$string['confirmpart2'] = '<br />in courses:<br />';
+$string['removeall'] = 'Remove all from selection';
+$string['selectedcourses'] = 'Selected courses';
+$string['title'] = 'Continue manual enrolments of users';
+
+?>
---
+++ actions/continuemanualenrolments/lang/ru/bulkuseractions_continuemanualenrolments.php
@@ -0,0 +1,12 @@
+<?PHP
+
+$string['pluginname'] = 'Возобновить ручную подписку пользователей';
+$string['allcourses'] = 'Все курсы';
+$string['accept'] = 'Принять';
+$string['confirmpart1'] = 'Вы уверены, что хотите возобновить ручную подписку пользователей:<br />';
+$string['confirmpart2'] = '<br />в курсах:<br />';
+$string['removeall'] = 'Удалить все из выбранных';
+$string['selectedcourses'] = 'Выбранные курсы';
+$string['title'] = 'Возобновить ручной подписки пользователей';
+
+?>
